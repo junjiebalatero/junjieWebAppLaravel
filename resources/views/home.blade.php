bc0ed10340e6be34ecd803d2d273a30249b94a88
Margarito <margaritobalatero@gmail.com>
2020-01-29 14:05:49 +0800

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://gateway.ipfs.io/ipfs/QmNgoacgbARoSn9vhAPYdDMzSnsow7dFCfHzeZKftJ1hRD" class="rounded-circle">
         </div>
+        <div class="col-9 pt-5">
+            <div><h1>junjie</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class=pr-5><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+           </div>
+            <div class="pt-4 font-weight-bold">junjie</div>
+            <div>Hello World!!!!!!!!!!!</div>
+            <div><a href="#">balatero.herokuapp.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://gateway.ipfs.io/ipfs/QmNgoacgbARoSn9vhAPYdDMzSnsow7dFCfHzeZKftJ1hRD" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://gateway.ipfs.io/ipfs/QmNgoacgbARoSn9vhAPYdDMzSnsow7dFCfHzeZKftJ1hRD" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://gateway.ipfs.io/ipfs/QmNgoacgbARoSn9vhAPYdDMzSnsow7dFCfHzeZKftJ1hRD" class="w-100">
+        </div>
+
     </div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    junjie
                 </div>
 
                 <div class="links">
